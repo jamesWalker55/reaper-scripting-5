d6c1ad2a5f3ace9d1147f1e05df2c805cd823b03
James Walker <james.chunho@gmail.com>
2024-07-17 21:44:23 +0800

    forgot to commit this

---
+++ lib/reaper-api/src/item.ts
@@ -0,0 +1,53 @@
+export class Item {
+  obj: MediaItem;
+
+  constructor(obj: MediaItem) {
+    this.obj = obj;
+  }
+
+  *iterTakes() {
+    const count = reaper.GetMediaItemNumTakes(this.obj);
+    for (let i = 0; i < count; i++) {
+      const take = reaper.GetMediaItemTake(this.obj, i);
+      yield new Take(take);
+    }
+  }
+
+  isEmpty() {
+    const count = reaper.GetMediaItemNumTakes(this.obj);
+    return count === 0;
+  }
+}
+
+export class Take {
+  obj: MediaItem_Take;
+
+  constructor(obj: MediaItem_Take) {
+    this.obj = obj;
+  }
+
+  type() {
+    const source = reaper.GetMediaItemTake_Source(this.obj);
+    return reaper.GetMediaSourceType(source);
+  }
+
+  isMidi() {
+    return this.type() === "MIDI";
+  }
+
+  source() {
+    return new Source(reaper.GetMediaItemTake_Source(this.obj));
+  }
+}
+
+export class Source {
+  obj: PCM_source;
+
+  constructor(obj: PCM_source) {
+    this.obj = obj;
+  }
+
+  type() {
+    return reaper.GetMediaSourceType(this.obj);
+  }
+}
