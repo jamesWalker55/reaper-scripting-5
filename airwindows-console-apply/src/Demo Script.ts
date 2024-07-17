76ec9fda944538044bd36f1841f41a9850c3d1e8
James Walker <james.chunho@gmail.com>
2024-07-17 15:01:49 +0800

    basics done

--- airwindows-console-apply/src/Demo Script.ts
+++ airwindows-console-apply/src/Demo Script.ts
@@ -26,137 +26,139 @@ function awcDbToScalar(db: number) {
   return dbToScalar(db) / 2;
 }
 
+/**
+ * This FX is expected to map 2 channels of input to the same output channels.
+ * Return the 2 channels if this is indeed the case. Otherwise, return null.
+ */
+function getMappedChannels(fx: TrackFX) {
+  // get the pin mapping bitmasks
+  const tr = fx.track;
+  const idx = fx.fxidx;
+  const [in1a, in1b] = reaper.TrackFX_GetPinMappings(tr, idx, 0, 0);
+  const [in1c, in1d] = reaper.TrackFX_GetPinMappings(tr, idx, 0, 0 + 0x1000000);
+  const [in2a, in2b] = reaper.TrackFX_GetPinMappings(tr, idx, 0, 1);
+  const [in2c, in2d] = reaper.TrackFX_GetPinMappings(tr, idx, 0, 1 + 0x1000000);
+  const [out1a, out1b] = reaper.TrackFX_GetPinMappings(tr, idx, 1, 0);
+  const [out1c, out1d] = reaper.TrackFX_GetPinMappings(
+    tr,
+    idx,
+    1,
+    0 + 0x1000000,
+  );
+  const [out2a, out2b] = reaper.TrackFX_GetPinMappings(tr, idx, 1, 1);
+  const [out2c, out2d] = reaper.TrackFX_GetPinMappings(
+    tr,
+    idx,
+    1,
+    1 + 0x1000000,
+  );
+
+  // ensure pin inputs/outputs are the same
+  {
+    const inOut1IsTheSame =
+      in1a === out1a && in1b === out1b && in1c === out1c && in1d === out1d;
+    const inOut2IsTheSame =
+      in2a === out2a && in2b === out2b && in2c === out2c && in2d === out2d;
+    if (!inOut1IsTheSame || !inOut2IsTheSame) {
+      return null;
+    }
+  }
+
+  // find the SINGLE pinned channel, otherwise return null
+  const pin1 = [in1a, in1b, in1c, in1d];
+  const pin2 = [in2a, in2b, in2c, in2d];
+  let pin1channel: number | null = null;
+  let pin2channel: number | null = null;
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 32; j++) {
+      const channelIdx = j + 32 * i;
+      const pin1isSet = (pin1[i] & (1 << j)) !== 0;
+      if (pin1isSet) {
+        if (pin1channel === null) {
+          pin1channel = channelIdx;
+        } else {
+          // more than 1 pin is set
+          return null;
+        }
+      }
+      const pin2isSet = (pin2[i] & (1 << j)) !== 0;
+      if (pin2isSet) {
+        if (pin2channel === null) {
+          pin2channel = channelIdx;
+        } else {
+          // more than 1 pin is set
+          return null;
+        }
+      }
+    }
+  }
+  if (pin1channel === null) return null;
+  if (pin2channel === null) return null;
+
+  // input/output channels should be different
+  if (pin1channel === pin2channel) return null;
+
+  // input/output channels should be sequential
+  if (!(pin1channel + 1 === pin2channel)) return null;
+
+  return [pin1channel, pin2channel] as [number, number];
+}
+
 class CStripFx {
-  FX_IDENT = "utility/volume_pan_sample_accurate_auto";
-  FX_NAME = "#CSTRIP [AIRWINDOWS CONSOLE]";
-  FX_ADD: AddFxParams = { js: this.FX_IDENT };
+  static FX_IDENT = "utility/volume_pan_sample_accurate_auto";
+  static FX_NAME = "#CSTRIP [AIRWINDOWS CONSOLE]";
+  static FX_ADD: AddFxParams = { js: CStripFx.FX_IDENT };
 
   fx: TrackFX;
   gainParam: FXParam;
   panParam: FXParam;
 
-  private constructor(fx: TrackFX) {
+  private constructor(fx: TrackFX, gainParam: FXParam, panParam: FXParam) {
     this.fx = fx;
     // values are in dB
-    this.gainParam = this.fx.getParameter(0);
+    this.gainParam = gainParam;
     // ranges from -100.0 .. 100.0
-    this.panParam = this.fx.getParameter(1);
-
-    this.validate();
+    this.panParam = panParam;
   }
 
-  validate() {
-    const fxName = this.fx.getName();
-    const fxIdent = this.fx.getIdent();
-    assert(fxName === this.FX_NAME);
-    assert(fxIdent === this.FX_IDENT);
+  static fromFx(fx: TrackFX): CStripFx | null {
+    const fxName = fx.getName();
+    const fxIdent = fx.getIdent();
+    if (!(fxName === CStripFx.FX_NAME && fxIdent === CStripFx.FX_IDENT)) {
+      return null;
+    }
+
+    // values are awc scalar 0 - 1
+    const gainParam = fx.getParameter(0);
+    if (!gainParam) return null;
+
+    // values are 0 - 1
+    const panParam = fx.getParameter(1);
+    if (!panParam) return null;
+
+    return new CStripFx(fx, gainParam, panParam);
   }
 
-  create(track: Track) {
+  static create(track: Track): CStripFx {
     const count = track.getFxCount();
-    const newPos = track.addFx(this.FX_ADD, count);
+    const newPos = track.addFx(CStripFx.FX_ADD, count);
     if (newPos === null) error("failed to create gain plugin");
 
     const fx = track.getFx(newPos);
-    return new CStripFx(fx);
+    fx.rename(CStripFx.FX_NAME);
+    const strip = CStripFx.fromFx(fx);
+    if (strip === null)
+      error("created gain plugin, but its configuration is invalid");
+
+    return strip;
   }
 
-  findAll(track: Track): CStripFx[] {
-    const result = [];
+  static findAll(track: Track): CStripFx | null {
     for (const fx of track.getAllFx().reverse()) {
-      if (fx.getIdent() === this.FX_IDENT && fx.getName() === this.FX_NAME) {
-        result.push(new CStripFx(fx));
-      }
+      const channel = CStripFx.fromFx(fx);
+      if (channel !== null) return channel;
     }
-    return result;
-  }
-
-  /**
-   * This FX is expected to map 2 channels of input to the same output channels.
-   * Return the 2 channels if this is indeed the case. Otherwise, return null.
-   */
-  getMappedChannels() {
-    // get the pin mapping bitmasks
-    const tr = this.fx.track;
-    const idx = this.fx.fxidx;
-    const [in1a, in1b] = reaper.TrackFX_GetPinMappings(tr, idx, 0, 0);
-    const [in1c, in1d] = reaper.TrackFX_GetPinMappings(
-      tr,
-      idx,
-      0,
-      0 + 0x1000000,
-    );
-    const [in2a, in2b] = reaper.TrackFX_GetPinMappings(tr, idx, 0, 1);
-    const [in2c, in2d] = reaper.TrackFX_GetPinMappings(
-      tr,
-      idx,
-      0,
-      1 + 0x1000000,
-    );
-    const [out1a, out1b] = reaper.TrackFX_GetPinMappings(tr, idx, 1, 0);
-    const [out1c, out1d] = reaper.TrackFX_GetPinMappings(
-      tr,
-      idx,
-      1,
-      0 + 0x1000000,
-    );
-    const [out2a, out2b] = reaper.TrackFX_GetPinMappings(tr, idx, 1, 1);
-    const [out2c, out2d] = reaper.TrackFX_GetPinMappings(
-      tr,
-      idx,
-      1,
-      1 + 0x1000000,
-    );
-
-    // ensure pin inputs/outputs are the same
-    {
-      const inOut1IsTheSame =
-        in1a === out1a && in1b === out1b && in1c === out1c && in1d === out1d;
-      const inOut2IsTheSame =
-        in2a === out2a && in2b === out2b && in2c === out2c && in2d === out2d;
-      if (!inOut1IsTheSame || !inOut2IsTheSame) {
-        return null;
-      }
-    }
-
-    // find the SINGLE pinned channel, otherwise return null
-    const pin1 = [in1a, in1b, in1c, in1d];
-    const pin2 = [in2a, in2b, in2c, in2d];
-    let pin1channel: number | null = null;
-    let pin2channel: number | null = null;
-    for (let i = 0; i < 4; i++) {
-      for (let j = 0; j < 32; j++) {
-        const channelIdx = j + 32 * i;
-        const pin1isSet = (pin1[i] & (1 << j)) !== 0;
-        if (pin1isSet) {
-          if (pin1channel === null) {
-            pin1channel = channelIdx;
-          } else {
-            // more than 1 pin is set
-            return null;
-          }
-        }
-        const pin2isSet = (pin2[i] & (1 << j)) !== 0;
-        if (pin2isSet) {
-          if (pin2channel === null) {
-            pin2channel = channelIdx;
-          } else {
-            // more than 1 pin is set
-            return null;
-          }
-        }
-      }
-    }
-    if (pin1channel === null) return null;
-    if (pin2channel === null) return null;
-
-    // input/output channels should be different
-    if (pin1channel === pin2channel) return null;
-
-    // input/output channels should be sequential
-    if (!(pin1channel + 1 === pin2channel)) return null;
-
-    return [pin1channel, pin2channel];
+    return null;
   }
 
   /** Gain amount in dB */
@@ -175,7 +177,7 @@ class CStripFx {
     return this.panParam.getValue().cur;
   }
 
-  /** @param gain Pan amount in range (-100 .. 100) */
+  /** @param pan Pan amount in range (-100 .. 100) */
   setPan(pan: number) {
     const rv = this.panParam.setValue(pan);
     assert(rv, "failed to set pan");
@@ -229,11 +231,12 @@ class ChannelFx {
     return new ChannelFx(fx, gainParam, panParam);
   }
 
-  create(track: Track) {
+  static create(track: Track) {
     const newPos = track.addFx(ChannelFx.FX_ADD);
     if (newPos === null) error("failed to create channel plugin");
 
     const fx = track.getFx(newPos);
+    fx.rename(ChannelFx.FX_NAME);
     const ok = reaper.TrackFX_SetPreset(
       fx.track,
       fx.fxidx,
@@ -251,7 +254,7 @@ class ChannelFx {
     return ChannelFx.fromFx(fx);
   }
 
-  find(track: Track): ChannelFx | null {
+  static find(track: Track): ChannelFx | null {
     for (const fx of track.getAllFx().reverse()) {
       const channel = ChannelFx.fromFx(fx);
       if (channel !== null) return channel;
@@ -259,115 +262,39 @@ class ChannelFx {
     return null;
   }
 
-  // /**
-  //  * This FX is expected to map 2 channels of input to the same output channels.
-  //  * Return the 2 channels if this is indeed the case. Otherwise, return null.
-  //  */
-  // getMappedChannels() {
-  //   // get the pin mapping bitmasks
-  //   const tr = this.fx.track;
-  //   const idx = this.fx.fxidx;
-  //   const [in1a, in1b] = reaper.TrackFX_GetPinMappings(tr, idx, 0, 0);
-  //   const [in1c, in1d] = reaper.TrackFX_GetPinMappings(
-  //     tr,
-  //     idx,
-  //     0,
-  //     0 + 0x1000000,
-  //   );
-  //   const [in2a, in2b] = reaper.TrackFX_GetPinMappings(tr, idx, 0, 1);
-  //   const [in2c, in2d] = reaper.TrackFX_GetPinMappings(
-  //     tr,
-  //     idx,
-  //     0,
-  //     1 + 0x1000000,
-  //   );
-  //   const [out1a, out1b] = reaper.TrackFX_GetPinMappings(tr, idx, 1, 0);
-  //   const [out1c, out1d] = reaper.TrackFX_GetPinMappings(
-  //     tr,
-  //     idx,
-  //     1,
-  //     0 + 0x1000000,
-  //   );
-  //   const [out2a, out2b] = reaper.TrackFX_GetPinMappings(tr, idx, 1, 1);
-  //   const [out2c, out2d] = reaper.TrackFX_GetPinMappings(
-  //     tr,
-  //     idx,
-  //     1,
-  //     1 + 0x1000000,
-  //   );
+  /** Gain amount in dB */
+  gain() {
+    const scalar = this.gainParam.getValue().cur;
+    return awcScalarToDb(scalar);
+  }
 
-  //   // ensure pin inputs/outputs are the same
-  //   {
-  //     const inOut1IsTheSame =
-  //       in1a === out1a && in1b === out1b && in1c === out1c && in1d === out1d;
-  //     const inOut2IsTheSame =
-  //       in2a === out2a && in2b === out2b && in2c === out2c && in2d === out2d;
-  //     if (!inOut1IsTheSame || !inOut2IsTheSame) {
-  //       return null;
-  //     }
-  //   }
+  /**
+   * NOTE: Gain is limited! Max gain is ~6dB.
+   * See `ChannelFx.GAIN_DB_MAX`
+   * @param gain Gain amount in dB
+   */
+  setGain(gain: number) {
+    if (gain > ChannelFx.GAIN_DB_MAX) {
+      error(`cannot set gain of channel plugin to ${gain} dB`);
+    }
+    const rv = this.gainParam.setValue(awcDbToScalar(gain));
+    assert(rv, "failed to set gain");
+  }
 
-  //   // find the SINGLE pinned channel, otherwise return null
-  //   const pin1 = [in1a, in1b, in1c, in1d];
-  //   const pin2 = [in2a, in2b, in2c, in2d];
-  //   let pin1channel: number | null = null;
-  //   let pin2channel: number | null = null;
-  //   for (let i = 0; i < 4; i++) {
-  //     for (let j = 0; j < 32; j++) {
-  //       const channelIdx = j + 32 * i;
-  //       const pin1isSet = (pin1[i] & (1 << j)) !== 0;
-  //       if (pin1isSet) {
-  //         if (pin1channel === null) {
-  //           pin1channel = channelIdx;
-  //         } else {
-  //           // more than 1 pin is set
-  //           return null;
-  //         }
-  //       }
-  //       const pin2isSet = (pin2[i] & (1 << j)) !== 0;
-  //       if (pin2isSet) {
-  //         if (pin2channel === null) {
-  //           pin2channel = channelIdx;
-  //         } else {
-  //           // more than 1 pin is set
-  //           return null;
-  //         }
-  //       }
-  //     }
-  //   }
-  //   if (pin1channel === null) return null;
-  //   if (pin2channel === null) return null;
+  /** Pan amount in range (-100 .. 100) */
+  pan() {
+    // range 0..1
+    const scalar = this.panParam.getValue().cur;
+    return scalar * 200 - 100;
+  }
 
-  //   // input/output channels should be different
-  //   if (pin1channel === pin2channel) return null;
-
-  //   // input/output channels should be sequential
-  //   if (!(pin1channel + 1 === pin2channel)) return null;
-
-  //   return [pin1channel, pin2channel];
-  // }
-
-  // /** Gain amount in dB */
-  // gain() {
-  //   return this.volParam.getValue().cur;
-  // }
-
-  // /** @param gain Gain amount in dB */
-  // setGain(gain: number) {
-  //   const rv = this.volParam.setValue(gain);
-  //   assert(rv, "failed to set gain");
-  // }
-
-  // /** Pan amount in range (-100 .. 100) */
-  // pan() {
-  //   return this.panParam.getValue().cur;
-  // }
-
-  // /** @param gain Pan amount in range (-100 .. 100) */
-  // setPan(pan: number) {
-  //   const rv = this.panParam.setValue(pan);
-  //   assert(rv, "failed to set pan");
-  // }
+  /** @param pan Pan amount in range (-100 .. 100) */
+  setPan(pan: number) {
+    // convert to range 0..1
+    const scalar = (pan + 100) / 200;
+    const rv = this.panParam.setValue(scalar);
+    assert(rv, "failed to set pan");
+  }
 }
 
 // class ConsoleTrack {
@@ -402,18 +329,10 @@ function log(msg: string) {
 }
 
 function main() {
-  const result = Track.getSelected()[0]
-    .getAllFx()
-    .map((x) => ({
-      ident: x.getIdent(),
-      name: x.getName(),
-      mapping: reaper.TrackFX_GetPreset(x.track, x.fxidx),
-      params: x.getParameters().map((p) => ({
-        getValue: p.getValue(),
-        getIdent: p.getIdent(),
-        getName: p.getName(),
-      })),
-    }));
+  const result = Track.getSelected().map((tr) => ({
+    // cstrips: CStripFx.findAll(tr),
+    channels: ChannelFx.find(tr),
+  }));
   log(inspect(result));
   copy(encode(result));
   // // map from raw track to track obj
