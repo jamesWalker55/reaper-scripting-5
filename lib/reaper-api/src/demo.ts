e1742b36673e984dc09a3370032f9e7dd1d87499
James Walker <james.chunho@gmail.com>
2024-08-18 14:32:53 +0800

    demo script

--- lib/reaper-api/src/demo.ts
+++ lib/reaper-api/src/demo.ts
@@ -6,31 +6,34 @@ import { getProjectRoutingInfo, Track } from "./track";
 import { copy } from "./clipboard";
 import { Item } from "./item";
 import * as Chunk from "./chunk";
-import * as ArrChunk from "./arrchunk";
-import { errorHandler, log } from "./utils";
+import * as ArrChunk from "./element";
+import { errorHandler, log, readFile, writeFile } from "./utils";
+
+function measureTime<T>(func: () => T): [number, T] {
+  const startTime = os.clock();
+  const rv = func();
+  const endTime = os.clock();
+  return [endTime - startTime, rv];
+}
 
 function main() {
-  const track = Track.getSelected()[0];
-  const allSource = [];
-  for (const item of track.iterItems()) {
-    const children = ArrChunk.fromChunk(Chunk.item(item.obj));
-    let source: string | null = null;
-    for (const child of children) {
-      if (typeof child === "string") continue;
-      const tag = child[0];
-      if (typeof tag !== "string") continue;
-      if (!tag.startsWith("SOURCE ")) continue;
-      const file = child[1];
-      if (typeof file !== "string") continue;
-      if (!file.startsWith("FILE")) continue;
-      source = file;
-    }
-    if (source === null) throw new Error("SOURCE not found in item");
+  const [track] = assert(Track.getSelected()[0], "no track selected");
+  const [elapsed, send] = measureTime(() =>
+    track
+      .getSends(true)
+      .map((x) => ({
+        audio: x.audio,
+        midi: x.midi,
+        src: x.src.getIdx(),
+        dst: x.dst.getIdx(),
+      })),
+  );
+  log("elapsed", elapsed);
 
-    allSource.push(source);
-  }
-  log(allSource);
-  // copy(allSource.join("\n"));
+  writeFile(
+    "D:\\Programming\\reaper-scripting-5\\lua_output.json",
+    encode(send),
+  );
 
   // const allVolpan = [];
   // for (const item of track.iterItems()) {
