39004d5fb896ca18beeb9e8ffc52d88968466870
James Walker <james.chunho@gmail.com>
2024-11-08 00:10:01 +0800

    new routing select scripts

---
+++ select-track-by-routing/src/Routing - Select receives from the selected track (Audio only).ts
@@ -0,0 +1,25 @@
+AddCwdToImportPaths();
+
+import { getProjectRoutingInfo, Track } from "reaper-api/track";
+
+function main() {
+  const { sends, receives } = getProjectRoutingInfo({ audio: true });
+
+  const idxToSelect: LuaTable<number, boolean> = new LuaTable();
+  for (const track of Track.getSelected()) {
+    const i = track.getIdx();
+    const senders = receives.get(i) || [];
+    for (const srcIdx of senders) {
+      idxToSelect.set(srcIdx, true);
+    }
+  }
+
+  const count = Track.count();
+  for (let i = 0; i < count; i++) {
+    const track = Track.getByIdx(i);
+    const selected = idxToSelect.has(i);
+    reaper.SetTrackSelected(track.obj, selected);
+  }
+}
+
+main();
