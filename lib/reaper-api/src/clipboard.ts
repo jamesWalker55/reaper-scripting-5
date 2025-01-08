170722b79b49dee640e649704a0f649e3edacc67
James Walker <james.chunho@gmail.com>
2025-01-08 13:01:28 +0800

    make clipboard check api on calling

--- lib/reaper-api/src/clipboard.ts
+++ lib/reaper-api/src/clipboard.ts
@@ -1,12 +1,22 @@
 import { ensureAPI } from "./utils";
 
-ensureAPI("SWS Extensions", "CF_SetClipboard");
-ensureAPI("SWS Extensions", "CF_GetClipboard");
+let apiChecked = false;
+
+function checkAPI() {
+  if (apiChecked) return;
+
+  ensureAPI("SWS Extensions", "CF_SetClipboard");
+  ensureAPI("SWS Extensions", "CF_GetClipboard");
+
+  apiChecked = true;
+}
 
 export function copy(text: string) {
+  checkAPI();
   reaper.CF_SetClipboard(text);
 }
 
 export function paste() {
+  checkAPI();
   return reaper.CF_GetClipboard();
 }
