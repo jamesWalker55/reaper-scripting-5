e506ae519b16beee73aa1a64d971865ead438f93
James Walker <james.chunho@gmail.com>
2025-01-25 14:24:44 +0800

    experimental new api

--- lib/reaper-api/src/extstate.ts
+++ lib/reaper-api/src/extstate.ts
@@ -1,3 +1,6 @@
+import * as JSON from "json";
+import { assertUnreachable } from "./utils";
+
 /**
  * Set a ext state. Defaults to permanent.
  *
@@ -62,6 +65,83 @@ export function Section(section: string) {
      */
     get(key: string) {
       return get(section, key);
+    },
+  };
+}
+
+type EncodeTypeName = "string" | "number" | "json";
+type EncodeTypeMap = {
+  string: string;
+  number: number;
+  json: object;
+};
+
+export function TypedSection<T extends Record<string, EncodeTypeName>>(
+  section: string,
+  config: T,
+) {
+  type ConfigType<K extends keyof T> = EncodeTypeMap[T[K]];
+
+  return {
+    /**
+     * Set a ext state.
+     *
+     * WARNING: An empty string is the same as setting as null!
+     */
+    set<K extends string & keyof T>(
+      key: K,
+      value: ConfigType<K> | null,
+      permanent?: boolean,
+    ) {
+      const configTypeName: EncodeTypeName = config[key];
+
+      switch (configTypeName) {
+        case "string": {
+          const typedValue = value as EncodeTypeMap["string"] | null;
+          set(section, key, typedValue, permanent);
+          break;
+        }
+        case "number": {
+          const typedValue = value as EncodeTypeMap["number"] | null;
+          set(section, key, typedValue?.toString() || null, permanent);
+          break;
+        }
+        case "json": {
+          const typedValue = value as EncodeTypeMap["json"] | null;
+          set(section, key, JSON.encode(typedValue), permanent);
+          break;
+        }
+        default:
+          assertUnreachable(configTypeName);
+      }
+    },
+    /**
+     * This returns a string or null.
+     *
+     * Conditions for returning null:
+     * - key is not set
+     * - key is set to an empty string (length 0)
+     */
+    get<K extends string & keyof T>(key: K): ConfigType<K> | null {
+      const configTypeName: EncodeTypeName = config[key];
+
+      switch (configTypeName) {
+        case "string": {
+          const value = get(section, key);
+          return value as ConfigType<K> | null;
+        }
+        case "number": {
+          const value = tonumber(get(section, key)) || null;
+          return value as ConfigType<K> | null;
+        }
+        case "json": {
+          const value = get(section, key);
+          if (value === null) return null;
+          return JSON.decode(value) as ConfigType<K> | null;
+        }
+        default:
+          assertUnreachable(configTypeName);
+      }
     },
   };
 }
