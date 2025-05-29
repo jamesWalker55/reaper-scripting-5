2e29ef7622d998e217842e8309db222a8068ee6d
James Walker <james.chunho@gmail.com>
2025-05-30 00:41:28 +0800

    new logic

--- item-bpm-helper/src/common.ts
+++ item-bpm-helper/src/common.ts
@@ -12,6 +12,10 @@ export const config = TypedSection("bpm-helper", {
   targetBpm: "number",
 });
 
+function clamp(min: number, x: number, max: number): number {
+  return Math.min(Math.max(min, x), max);
+}
+
 export function applyNewBpmToSelectedItems(
   sourceBpm: number,
   targetBpm: number,
@@ -26,28 +30,42 @@ export function applyNewBpmToSelectedItems(
         if (take.TYPE !== "AUDIO") continue;
       }
 
+      // store item fades to restore later
+      const fadeInLength = item.fadeInLength;
+      const fadeOutLength = item.fadeOutLength;
+
+      // old item info
+      const oldPos = item.position;
+      const oldLength = item.length;
+      const oldSnapOffset = item.snapOffset;
+      const oldSnapPos = oldPos + oldSnapOffset;
+
+      // calculate target position/length stuff
+      const newLength = (oldLength * sourceBpm) / targetBpm;
+      const newSnapOffset = clamp(
+        0,
+        reversed ? oldSnapOffset + newLength - oldLength : oldSnapOffset,
+        newLength,
+      );
+      const newPos = oldSnapPos - newSnapOffset;
+      item.position = newPos;
+      item.length = newLength;
+      item.snapOffset = newSnapOffset;
+
+      // if the item is reversed, slide take audio to preserve end transient
       if (reversed) {
-        // item lengths
-        const oldItemLength = item.length;
-        const newItemLength = (item.length * sourceBpm) / targetBpm;
+        for (const x of item.iterTakes()) {
+          const take = x.asTypedTake();
+          if (take.TYPE !== "AUDIO") continue;
 
-        // find new item position
-        const newItemPos = item.position + oldItemLength - newItemLength;
-
-        // store item fades to restore later
-        const fadeInLength = item.fadeInLength;
-        const fadeOutLength = item.fadeOutLength;
-
-        // split the item to trim the left side
-        const rightItem = item.split(newItemPos);
-        rightItem.fadeInLength = fadeInLength;
-        rightItem.fadeOutLength = fadeOutLength;
-
-        // delete left item
-        item.delete();
-      } else {
-        item.length = (item.length * sourceBpm) / targetBpm;
+          take.sourceStartOffset =
+            take.sourceStartOffset + (oldLength - newLength) * take.playrate;
+        }
       }
+
+      // restore item fades
+      item.fadeInLength = fadeInLength;
+      item.fadeOutLength = fadeOutLength;
     }
   });
 
