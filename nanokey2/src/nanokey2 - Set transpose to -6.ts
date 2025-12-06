9f645d0b309f332a195bafbc7ef193a6128d0e62
James Walker <james.chunho@gmail.com>
2025-12-07 04:26:35 +0800

    more scripts

---
+++ nanokey2/src/nanokey2 - Set transpose to -6.ts
@@ -0,0 +1,11 @@
+AddCwdToImportPaths();
+
+import { errorHandler } from "reaper-api/utils";
+import { loadScene, pushSceneToDevice, saveScene } from "./lib";
+
+errorHandler(() => {
+  const scene = loadScene();
+  scene.transpose = 64 - 6;
+  saveScene(scene);
+  pushSceneToDevice(scene);
+});
