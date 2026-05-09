6768e61c75927bd0ce4aca0aa84218d4dc07e5f1
James Walker <james.chunho@gmail.com>
2026-05-09 14:52:26 +0800

    simple resolve module

---
+++ key-scale-track/src/key/resolve.ts
@@ -0,0 +1,34 @@
+import { Key, Mode, Pitch, ScaleNote, wrapPitch } from "./types";
+
+const SCALE_NOTE_INTERVALS: Record<
+  Mode,
+  [number, number, number, number, number, number]
+> = {
+  [Mode.Ionian]: [2, 2, 1, 2, 2, 2],
+  [Mode.Dorian]: [2, 1, 2, 2, 2, 1],
+  [Mode.Phrygian]: [1, 2, 2, 2, 1, 2],
+  [Mode.Lydian]: [2, 2, 2, 1, 2, 2],
+  [Mode.Mixolydian]: [2, 2, 1, 2, 2, 1],
+  [Mode.Aeolian]: [2, 1, 2, 2, 1, 2],
+  [Mode.Locrian]: [1, 2, 2, 1, 2, 2],
+};
+
+/** The 1st pitch in the list is the root, 2nd pitch is 2nd scale note, etc */
+export function keyToPitches(key: Key): Pitch[] {
+  const rootPitch = key.tonic;
+  const pitches = SCALE_NOTE_INTERVALS[key.mode].reduce(
+    (acc, interval) => {
+      const nextPitch = wrapPitch(acc[acc.length - 1]! + interval);
+      acc.push(nextPitch);
+      return acc;
+    },
+    [rootPitch],
+  );
+  if (key.alt) {
+    for (let i = 0; i <= 7; i++) {
+      const amt = key.alt[i as ScaleNote] || 0;
+      pitches[i as ScaleNote]! += amt;
+    }
+  }
+  return pitches;
+}
