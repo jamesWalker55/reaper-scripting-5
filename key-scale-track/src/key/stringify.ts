66f023396152a295aed8e68a7bacb6fdec1b30f5
James Walker <james.chunho@gmail.com>
2026-05-09 18:49:13 +0800

    wip new stringify

--- key-scale-track/src/key/stringify.ts
+++ key-scale-track/src/key/stringify.ts
@@ -1,58 +1,41 @@
-import { Key, Mode, ModeAlt, Pitch, ScaleNote } from "./types";
+import { log } from "reaper-api/utils";
+import { keyToPitches } from "./resolve";
+import { Key, Mode, ModeAlt, Pitch, ScaleNote, wrapPitch } from "./types";
 
-function pitchToString(x: Pitch): string {
-  switch (x) {
-    case Pitch.C:
-      return "C";
-    case Pitch.CS:
-      return "C#";
-    case Pitch.D:
-      return "D";
-    case Pitch.DS:
-      return "D#";
-    case Pitch.E:
-      return "E";
-    case Pitch.F:
-      return "F";
-    case Pitch.FS:
-      return "F#";
-    case Pitch.G:
-      return "G";
-    case Pitch.GS:
-      return "G#";
-    case Pitch.A:
-      return "A";
-    case Pitch.AS:
-      return "A#";
-    case Pitch.B:
-      return "B";
-    default:
-      x satisfies never;
-      throw new Error("unreachable");
-  }
-}
-
-function modeToString(x: Mode): string {
-  switch (x) {
-    case Mode.Ionian:
-      return "";
-    case Mode.Dorian:
-      return " Dorian";
-    case Mode.Phrygian:
-      return " Phrygian";
-    case Mode.Lydian:
-      return " Lydian";
-    case Mode.Mixolydian:
-      return " Mixolydian";
-    case Mode.Aeolian:
-      return "m";
-    case Mode.Locrian:
-      return " Locrian";
-    default:
-      x satisfies never;
-      throw new Error("unreachable");
-  }
-}
+const PITCHES_NORMAL = {
+  [Pitch.C]: "C",
+  [Pitch.D]: "D",
+  [Pitch.E]: "E",
+  [Pitch.F]: "F",
+  [Pitch.G]: "G",
+  [Pitch.A]: "A",
+  [Pitch.B]: "B",
+};
+type NormalPitch = keyof typeof PITCHES_NORMAL;
+type AbnormalPitch = Exclude<Pitch, NormalPitch>;
+const PITCHES_SHARP: Record<AbnormalPitch, string> = {
+  [Pitch.CS]: "C#",
+  [Pitch.DS]: "D#",
+  [Pitch.FS]: "F#",
+  [Pitch.GS]: "G#",
+  [Pitch.AS]: "A#",
+};
+const PITCHES_FLAT: Record<AbnormalPitch, string> = {
+  [Pitch.CS]: "Db",
+  [Pitch.DS]: "Eb",
+  [Pitch.FS]: "Gb",
+  [Pitch.GS]: "Ab",
+  [Pitch.AS]: "Bb",
+};
+const MODES: Record<Mode, string> = {
+  [Mode.Ionian]: "",
+  [Mode.Dorian]: " Dorian",
+  [Mode.Phrygian]: " Phrygian",
+  [Mode.Lydian]: " Lydian",
+  [Mode.Mixolydian]: " Mixolydian",
+  [Mode.Aeolian]: "m",
+  [Mode.Locrian]: " Locrian",
+};
 
 function altToString(modeAlt: ModeAlt): string {
   const parts: string[] = [];
@@ -66,9 +49,80 @@ function altToString(modeAlt: ModeAlt): string {
   return parts.join(" ");
 }
 
+/** if the start pitch is sharp, then it will be skipped */
+function* iterNonSharpPitches(start: Pitch) {
+  for (let i = 0; i < 12; i++) {
+    const pitch = wrapPitch(start + i);
+    if (pitch in PITCHES_NORMAL) {
+      yield pitch;
+    }
+  }
+}
+
+function zipNonSharpPitches(start: Pitch, zip: Pitch[]) {
+  if (zip.length !== 7)
+    throw new Error("expected exactly 7 pitches in a scale");
+
+  const nonSharp = [];
+  for (const p of iterNonSharpPitches(start)) {
+    nonSharp.push(p);
+  }
+  if (nonSharp.length !== 7)
+    throw new Error("expected exactly 7 non-sharp pitches");
+
+  return nonSharp.map((nonsharp, i) => ({ nonsharp, zip: zip[i]! }));
+}
+
+/**
+ * basically, check each letter and see which letter will produce the least sharp/flats in the scale note names
+ *
+ * https://en.wikipedia.org/wiki/Letter_notation#Choice_of_note_names
+ */
+function tonicToStringDiatonic(tonic: Pitch, pitches: Pitch[]): string {
+  if (tonic in PITCHES_NORMAL) return PITCHES_NORMAL[tonic as NormalPitch];
+
+  type AltCount = { total: number; max: number };
+
+  // try sharp notation "<P-1>#"
+  const sharpInfo: AltCount = { total: 0, max: 0 };
+  for (const { nonsharp, zip } of zipNonSharpPitches(
+    wrapPitch(tonic - 1),
+    pitches,
+  )) {
+    const distance = Math.abs(nonsharp - zip);
+    sharpInfo.total += distance;
+    sharpInfo.max = Math.max(sharpInfo.max, distance);
+  }
+
+  // try flat notation "<P+1>b"
+  const flatInfo: AltCount = { total: 0, max: 0 };
+  for (const { nonsharp, zip } of zipNonSharpPitches(
+    wrapPitch(tonic + 1),
+    pitches,
+  )) {
+    const distance = Math.abs(nonsharp - zip);
+    flatInfo.total += distance;
+    flatInfo.max = Math.max(flatInfo.max, distance);
+  }
+
+  log(sharpInfo, flatInfo);
+
+  if (sharpInfo.max > flatInfo.max) {
+    return PITCHES_FLAT[tonic as AbnormalPitch];
+  } else if (sharpInfo.max < flatInfo.max) {
+    return PITCHES_SHARP[tonic as AbnormalPitch];
+  } else if (sharpInfo.total > flatInfo.total) {
+    return PITCHES_FLAT[tonic as AbnormalPitch];
+  } else if (sharpInfo.total < flatInfo.total) {
+    return PITCHES_SHARP[tonic as AbnormalPitch];
+  } else {
+    return PITCHES_SHARP[tonic as AbnormalPitch];
+  }
+}
+
 export function stringifyKey(key: Key): string {
-  const tonic = pitchToString(key.tonic);
-  const mode = modeToString(key.mode);
+  const tonic = tonicToStringDiatonic(key.tonic, keyToPitches(key));
+  const mode = MODES[key.mode];
   const alt = key.alt ? ` ${altToString(key.alt)}` : "";
   return `${tonic}${mode}${alt}`;
 }
