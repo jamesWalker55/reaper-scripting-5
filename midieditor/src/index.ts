0d6b6332bf3211cb744df9b45d94eab5cd710a79
James Walker <james.chunho@gmail.com>
2026-05-17 17:41:15 +0800

    asdfghjkl

--- midieditor/src/index.ts
+++ midieditor/src/index.ts
@@ -5,6 +5,10 @@ import { createContext, microUILoop, Option, ReaperContext } from "microui";
 import { baseWindow } from "./widgets";
 import { Item } from "reaper-api/track";
 import * as MB from "reaper-api/midibuf";
+import * as json from "reaper-api/json";
+import { getDefault, getTimeMarkers } from "./timesections";
+import { timeToBeats } from "./timemap";
+import { inspect } from "reaper-api/inspect";
 
 function setWindowTitle(title: string) {
   gfx.init(title);
@@ -20,32 +24,42 @@ function main() {
     baseWindow(ctx, () => {
       ctx.layoutRow([-1], 0);
 
-      const items = Item.getSelected()
-        .map((item) => {
-          const take = item.activeTake()?.asTypedTake() || null;
-          if (take === null) return null;
+      ctx.label(`timeToBeats(0, reaper.GetCursorPositionEx(0))`);
+      ctx.text(inspect(timeToBeats(0, reaper.GetCursorPositionEx(0))));
 
-          return { item, take };
-        })
-        .filter((x) => x !== null);
-
-      for (const { item, take } of items) {
-        switch (take.TYPE) {
-          case "AUDIO": {
-            ctx.label(
-              `Audio: ${take.getSource().findRootParent().getFilename()}`,
-            );
-            break;
-          }
-          case "MIDI": {
-            const evts = MB.parseBuf(take.midibuf);
-            ctx.label(`Midi: ${evts.length} events`);
-            break;
-          }
-          default:
-            take satisfies never;
-        }
+      const info = getDefault(0);
+      ctx.label(json.encode(info));
+      const markers = getTimeMarkers(0);
+      for (const x of markers) {
+        ctx.label(json.encode(x));
       }
+
+      // const items = Item.getSelected()
+      //   .map((item) => {
+      //     const take = item.activeTake()?.asTypedTake() || null;
+      //     if (take === null) return null;
+
+      //     return { item, take };
+      //   })
+      //   .filter((x) => x !== null);
+
+      // for (const { item, take } of items) {
+      //   switch (take.TYPE) {
+      //     case "AUDIO": {
+      //       ctx.label(
+      //         `Audio: ${take.getSource().findRootParent().getFilename()}`,
+      //       );
+      //       break;
+      //     }
+      //     case "MIDI": {
+      //       const evts = MB.parseBuf(take.midibuf);
+      //       ctx.label(`Midi: ${evts.length} events`);
+      //       break;
+      //     }
+      //     default:
+      //       take satisfies never;
+      //   }
+      // }
     });
   });
 }
---
+++ midieditor/src/timemap.ts
@@ -0,0 +1,45 @@
+import { inspect } from "reaper-api/inspect";
+
+export function dividedBpmAtTime(proj: ReaProject, pos: number): number {
+  return reaper.TimeMap2_GetDividedBpmAtTime(proj, pos);
+}
+
+export function nextChangeTime(proj: ReaProject, afterPos: number): number {
+  return reaper.TimeMap2_GetNextChangeTime(proj, afterPos);
+}
+
+export function qnToTime(proj: ReaProject, qn: number): number {
+  return reaper.TimeMap2_QNToTime(proj, qn);
+}
+
+export function timeToQn(proj: ReaProject, time: number): number {
+  return reaper.TimeMap2_timeToQN(proj, time);
+}
+
+/** measures must be integer */
+export function beatsToTime(
+  proj: ReaProject,
+  beats: number,
+  measures?: number,
+): number {
+  return reaper.TimeMap2_beatsToTime(proj, beats, measures);
+}
+
+export function timeToBeats(proj: ReaProject, time: number) {
+  const [beats, measures, timesig_num, fullbeats, timesig_denom] =
+    reaper.TimeMap2_timeToBeats(proj, time);
+
+  // documentation says all params may be null but i don't know when
+  if (
+    measures === null ||
+    timesig_num === null ||
+    fullbeats === null ||
+    timesig_denom === null
+  ) {
+    throw new Error(
+      `failed to convert time to beats reaper.TimeMap2_timeToBeats(${inspect(proj)}, ${inspect(time)})`,
+    );
+  }
+
+  return { beats, measures, timesig_num, fullbeats, timesig_denom };
+}
